084c4f2b6f2a90ba12ff051a6f807b439e7c0499
Mani Teja <a-9795@ibsplc.com>
2021-10-05 23:45:16 +0530

    new changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import "./header.styles.scss"
-import Logo from "../../assets/elisrun logo.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import MailIcon from "@mui/icons-material/Mail";
+import "./header.styles.scss";
+import Logo from "../../assets/rexcloud_logo.svg";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="header-wrapper">
-                <Link to="/">
-                    <img src={Logo} alt="" className="logo-container"/>
-                </Link>
+  return (
+    <div className="header">
+      <div className="header-wrapper">
+        <Link to="/">
+          <img src={Logo} alt="" className="logo-container" />
+        </Link>
 
-                <Link to="/contact">
-                    <span className="contact-text">Contact</span>
-                </Link>
-            </div>
-        </div>
-    );
-}
+        <Link to="/contact" className="contact-link">
+          <MailIcon style={{ width: "16px" }} />
+          <span className="contact-text">Contact</span>
+        </Link>
+      </div>
+      <div className="header-dropdown-wrapper">
+        <Link to="/">Option</Link>
+        <Link to="/">Option</Link>
+        <Link to="/">Option</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
