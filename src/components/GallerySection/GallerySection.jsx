d02b6eede2a544d1cb2da7b6fce5e1cd82415f2e
Mani.Teja <mani.teja@ibsplc.com>
2021-10-08 01:17:30 +0530

    Slideshow on Gallery Section of Homepage

--- src/components/GallerySection/GallerySection.jsx
+++ src/components/GallerySection/GallerySection.jsx
@@ -1,7 +1,70 @@
 import React from 'react';
+import { Box } from '@mui/system';
+import { Slide } from 'react-slideshow-image';
+import slide1 from '../../assets/slideshow1.jpg';
+import slide2 from '../../assets/slideshow2.jpg';
+import slide3 from '../../assets/slideshow3.jpg';
+import './gallerysection.styles.scss';
+import 'react-slideshow-image/dist/styles.css';
 
 function GallerySection() {
-  return <div>Hola!</div>;
+  const slideRef = React.createRef();
+  const [state, setState] = React.useState();
+
+  const back = () => {
+    slideRef.current.goBack();
+  };
+
+  const next = () => {
+    slideRef.current.goNext();
+  };
+
+  const properties = {
+    duration: 2000,
+    autoplay: true,
+    transitionDuration: 1000,
+    arrows: true,
+    infinite: true,
+    easing: 'ease',
+    indicators: () => <Box className="indicator"></Box>,
+  };
+
+  const slideImages = [slide1, slide2, slide3];
+  const slideInfo = [
+    {
+      heading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      heading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      heading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+  ];
+  return (
+    <Box className="slideshow">
+      <Box className="slide-container">
+        <Slide ref={slideRef} {...properties}>
+          {slideImages.map((each, index) => (
+            <Box key={index} className="each-slide">
+              <img className="lazy" src={each} alt="slide" />
+              {slideInfo[index] ? (
+                <Box className="slide-info">
+                  <h3>{slideInfo[index].heading}</h3>
+                  <p>{slideInfo[index].body}</p>
+                </Box>
+              ) : (
+                <> </>
+              )}
+            </Box>
+          ))}
+        </Slide>
+      </Box>
+    </Box>
+  );
 }
 
 export default GallerySection;
