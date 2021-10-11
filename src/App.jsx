c7e0754499573155e953d09d619cbd0a7dc8d3e8
Mani.Teja <mani.teja@ibsplc.com>
2021-10-11 20:18:25 +0530

    scrollbar styles for full app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import './App.css'
-import Header from './components/Header/Header'
-import HomePage from './pages/HomePage/HomePage'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-
-      </Switch>
-    </div>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
