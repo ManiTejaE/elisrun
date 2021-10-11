87905f41dda4029d2bfe7c8b42e50c162b9bef94
Mani.Teja <mani.teja@ibsplc.com>
2021-10-11 20:21:37 +0530

    added 3 sections

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,19 @@
-import { Box } from '@mui/system';
 import React from 'react';
+import { Box } from '@mui/system';
 import GallerySection from '../../components/GallerySection/GallerySection';
+import WhyWeSection from '../../components/WhyWeSection/WhyWeSection';
 
 import './homepage.styles.scss';
+import OurClientsSection from '../../components/OurClientsSection/OurClientsSection';
+import TestimonialsSection from '../../components/TestimonialsSection/TestimonialsSection';
 
 function HomePage() {
   return (
     <Box className="home-page">
       <GallerySection />
+      <WhyWeSection />
+      <OurClientsSection />
+      <TestimonialsSection />
     </Box>
   );
 }
